b511c43c612938ce93daf916abc29bac983db565
blackcrown <eunwoosunny@gmail.com>
2025-10-23 09:04:55 +0900

    update

--- review.js
+++ review.js
@@ -1,11 +1,3 @@
-const houseworks = [
-  "세탁기돌리기",
-  "방청소하기",
-  "옷정리하기",
-  "설거지하기",
-  "환기시키기",
-];
-for (let i = 5; i > -1; i--) {
-  let print = `${houseworks[i]},${i + 1}번`;
-  console.log(print);
-}
+const age = 27;
+const msg = (age >= 18 && "입장") || "불가";
+console.log(msg);
